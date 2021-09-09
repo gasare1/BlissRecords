b57f2e89799fca3a7390c9ee17fed567c7b102cc
Glen Asare <gasare1@asu.edu>
2021-09-08 21:24:38 -0700

    update

--- src/Components/Pages/HomepageCont.js
+++ src/Components/Pages/HomepageCont.js
@@ -1,10 +1,64 @@
 import React from 'react'
-import {Home,Background} from '../styled-components/HomeCont'
+import { Home, Background } from '../styled-components/HomeCont'
+import { Card, Button } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import image1 from '../../Images/goldenrecord.jpg'
+import { MDBCard, MDBCardBody, MDBCardTitle, MDBCardText, MDBCardImage, MDBBtn } from 'mdb-react-ui-kit';
 function HomepageCont() {
     return (
         <Home>
-            
+
+            <Card style={{ width: '100%', marginLeft: '10rem', height: '100%', marginTop: '100px', marginRight: '100px', marginLeft: '100px', justifyContent: 'left', alignItems: 'left', display: 'flex',flexDirection: 'row' }}>
+                <Card.Body>
+                    <MDBCard style={{ maxWidth: '22rem' }}>
+                        <MDBCardImage src='https://mdbcdn.b-cdn.net/img/new/standard/nature/184.jpg' position='top' alt='...' />
+                        <MDBCardBody>
+                            <MDBCardTitle>Card title</MDBCardTitle>
+                            <MDBCardText>
+                                Some quick example text to build on the card title and make up the bulk of the card's content.
+                            </MDBCardText>
+                            <MDBBtn href='#'>Button</MDBBtn>
+                        </MDBCardBody>
+                    </MDBCard>
+                </Card.Body>
+                <Card.Body >
+                    <MDBCard style={{ maxWidth: '22rem' }}>
+                        <MDBCardImage src='https://mdbcdn.b-cdn.net/img/new/standard/nature/184.jpg' position='top' alt='...' />
+                        <MDBCardBody>
+                            <MDBCardTitle>Card title</MDBCardTitle>
+                            <MDBCardText>
+                                Some quick example text to build on the card title and make up the bulk of the card's content.
+                            </MDBCardText>
+                            <MDBBtn href='#'>Button</MDBBtn>
+                        </MDBCardBody>
+                    </MDBCard>
+                </Card.Body>
+                <Card.Body >
+                    <MDBCard style={{ maxWidth: '22rem' }}>
+                        <MDBCardImage src='https://mdbcdn.b-cdn.net/img/new/standard/nature/184.jpg' position='top' alt='...' />
+                        <MDBCardBody>
+                            <MDBCardTitle>Card title</MDBCardTitle>
+                            <MDBCardText>
+                                Some quick example text to build on the card title and make up the bulk of the card's content.
+                            </MDBCardText>
+                            <MDBBtn href='#'>Button</MDBBtn>
+                        </MDBCardBody>
+                    </MDBCard>
+                </Card.Body>
+                <Card.Body >
+                    <MDBCard style={{ maxWidth: '22rem' }}>
+                        <MDBCardImage src='https://mdbcdn.b-cdn.net/img/new/standard/nature/184.jpg' position='top' alt='...' />
+                        <MDBCardBody>
+                            <MDBCardTitle>Card title</MDBCardTitle>
+                            <MDBCardText>
+                                Some quick example text to build on the card title and make up the bulk of the card's content.
+                            </MDBCardText>
+                            <MDBBtn href='#'>Button</MDBBtn>
+                        </MDBCardBody>
+                    </MDBCard>
+                </Card.Body>
+            </Card>
+
         </Home>
     )
 }
--- src/Components/styled-components/HomeCont.js
+++ src/Components/styled-components/HomeCont.js
@@ -1,8 +1,15 @@
 import styled from "styled-components";
 
 export const Home = styled.div`
+margin-top:40px;
+width:100%;
+z-index:1;
+justify-content:center;
+align-items:center;
+float:center;
+margin-right:30rem;
 
-z-index:-1;
+display:flex;
 `
 export const Background = styled.img`
 

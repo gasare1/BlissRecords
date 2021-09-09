f8aea68b5080786226aa2a18d268c1899a7bd9d1
Glen Asare <gasare1@asu.edu>
2021-09-08 23:04:40 -0700

    update

--- src/Components/Pages/HomepageCont.js
+++ src/Components/Pages/HomepageCont.js
@@ -1,65 +1,92 @@
 import React from 'react'
-import { Home, Background } from '../styled-components/HomeCont'
+import { Home, Background, Homelg, Card1 } from '../styled-components/HomeCont'
 import { Card, Button } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import image1 from '../../Images/goldenrecord.jpg'
-import { MDBCard, MDBCardBody, MDBCardTitle, MDBCardText, MDBCardImage, MDBBtn } from 'mdb-react-ui-kit';
+import { AiFillPlayCircle } from 'react-icons/ai'
+import { MDBCard, MDBCardBody, MDBCardTitle, MDBCardText, MDBCardImage, MDBBtn, MDBRipple } from 'mdb-react-ui-kit';
+import ReactAudioPlayer from 'react-audio-player';
+import Sarkodie from '../../Images/sarkodie.jpg'
+import Davido from '../../Images/Davido-2.jpg'
+import Sark from '../../Audio/[BTCLOD.COM] Sarkodie - Rollies and Cigars (Official Music Video)-320k.mp3'
+import Jowo from '../../Audio/[BTCLOD.COM] Davido - Jowo (Official Audio)-320k.mp3'
+import Fameye from '../../Images/Fameye.jpg'
+import PraiseFameye from '../../Audio/[BTCLOD.COM] Fameye - PRAISE (Audio Slide)-320k (1).mp3'
+import BlackSherrif from '../../Images/BlackSherrif.jpg'
+import SecondSermon from '../../Audio/[BTCLOD.COM] Black Sherif - Second Sermon (Official Video)-320k.mp3'
 function HomepageCont() {
     return (
         <Home>
+            <Homelg >
+                <MDBCard style={{ maxWidth: '22rem' }}>
+                    <MDBRipple rippleColor='light' rippleTag='div' className='bg-image hover-overlay'>
+                        <MDBCardImage src={Sarkodie} style={{maxHeight:"15rem",width:'22rem'}} fluid alt='...' />
+                        <a>
+                            <div className='mask' style={{ backgroundColor: 'rgba(251, 251, 251, 0.15)' }}></div>
+                        </a>
+                    </MDBRipple>
+                    <MDBCardBody>
+                        <MDBCardTitle> Sarkodie: Rollies and Cigars</MDBCardTitle>
+                        <ReactAudioPlayer
+                            src={Sark}
+                            autoPlay
+                            controls
+                        />
+                        We do not own the music in this video/rights to this music.
+                    </MDBCardBody>
+                </MDBCard>
+                <MDBCard style={{ maxWidth: '22rem', margin: '20px' }}>
+                    <MDBRipple rippleColor='light' rippleTag='div' className='bg-image hover-overlay'>
+                        <MDBCardImage src={Davido} style={{maxHeight:"15rem",width:'22rem'}}fluid alt='...' />
+                        <a>
+                            <div className='mask' style={{ backgroundColor: 'rgba(251, 251, 251, 0.15)' }}></div>
+                        </a>
+                    </MDBRipple>
+                    <MDBCardBody>
+                        <MDBCardTitle>Davido: Jowo</MDBCardTitle>
+                        <ReactAudioPlayer
+                            src={Jowo}
+                            controls
+                        />
+                        We do not own the music in this video/rights to this music.
+                    </MDBCardBody>
+                </MDBCard>
+                <MDBCard style={{ maxWidth: '22rem', margin: '20px' }}>
+                    <MDBRipple rippleColor='light' rippleTag='div' className='bg-image hover-overlay'>
+                        <MDBCardImage src={Fameye} fluid alt='...' style={{maxHeight:"15rem",width:'22rem'}}/>
+                        <a>
+                            <div className='mask' style={{ backgroundColor: 'rgba(251, 251, 251, 0.15)' }}></div>
+                        </a>
+                    </MDBRipple>
+                    <MDBCardBody>
+                        <MDBCardTitle>Fameye: Praise </MDBCardTitle>
+                        <ReactAudioPlayer
+                            src={PraiseFameye}
+                            controls
+                        />
+                        We do not own the music in this video/rights to this music.
+                    </MDBCardBody>
+                </MDBCard>
+                <MDBCard style={{ maxWidth: '22rem', margin: '20px' }}>
+                    <MDBRipple rippleColor='light' rippleTag='div' className='bg-image hover-overlay'>
+                        <MDBCardImage src={BlackSherrif} style={{maxHeight:"15rem",width:'22rem'}} fluid alt='...' />
+                        <a>
+                            <div className='mask' style={{ backgroundColor: 'rgba(251, 251, 251, 0.15)' }}></div>
+                        </a>
+                    </MDBRipple>
+                    <MDBCardBody>
+                        <MDBCardTitle>Card title</MDBCardTitle>
+                        <ReactAudioPlayer
+                            src={SecondSermon}
+                    
+                            controls
+                        />
+                        We do not own the music in this video/rights to this music.
+                    </MDBCardBody>
+                </MDBCard>
+            </Homelg>
 
-            <Card style={{ width: '100%', marginLeft: '10rem', height: '100%', marginTop: '100px', marginRight: '100px', marginLeft: '100px', justifyContent: 'left', alignItems: 'left', display: 'flex',flexDirection: 'row' }}>
-                <Card.Body>
-                    <MDBCard style={{ maxWidth: '22rem' }}>
-                        <MDBCardImage src='https://mdbcdn.b-cdn.net/img/new/standard/nature/184.jpg' position='top' alt='...' />
-                        <MDBCardBody>
-                            <MDBCardTitle>Card title</MDBCardTitle>
-                            <MDBCardText>
-                                Some quick example text to build on the card title and make up the bulk of the card's content.
-                            </MDBCardText>
-                            <MDBBtn href='#'>Button</MDBBtn>
-                        </MDBCardBody>
-                    </MDBCard>
-                </Card.Body>
-                <Card.Body >
-                    <MDBCard style={{ maxWidth: '22rem' }}>
-                        <MDBCardImage src='https://mdbcdn.b-cdn.net/img/new/standard/nature/184.jpg' position='top' alt='...' />
-                        <MDBCardBody>
-                            <MDBCardTitle>Card title</MDBCardTitle>
-                            <MDBCardText>
-                                Some quick example text to build on the card title and make up the bulk of the card's content.
-                            </MDBCardText>
-                            <MDBBtn href='#'>Button</MDBBtn>
-                        </MDBCardBody>
-                    </MDBCard>
-                </Card.Body>
-                <Card.Body >
-                    <MDBCard style={{ maxWidth: '22rem' }}>
-                        <MDBCardImage src='https://mdbcdn.b-cdn.net/img/new/standard/nature/184.jpg' position='top' alt='...' />
-                        <MDBCardBody>
-                            <MDBCardTitle>Card title</MDBCardTitle>
-                            <MDBCardText>
-                                Some quick example text to build on the card title and make up the bulk of the card's content.
-                            </MDBCardText>
-                            <MDBBtn href='#'>Button</MDBBtn>
-                        </MDBCardBody>
-                    </MDBCard>
-                </Card.Body>
-                <Card.Body >
-                    <MDBCard style={{ maxWidth: '22rem' }}>
-                        <MDBCardImage src='https://mdbcdn.b-cdn.net/img/new/standard/nature/184.jpg' position='top' alt='...' />
-                        <MDBCardBody>
-                            <MDBCardTitle>Card title</MDBCardTitle>
-                            <MDBCardText>
-                                Some quick example text to build on the card title and make up the bulk of the card's content.
-                            </MDBCardText>
-                            <MDBBtn href='#'>Button</MDBBtn>
-                        </MDBCardBody>
-                    </MDBCard>
-                </Card.Body>
-            </Card>
-
-        </Home>
+        </Home >
     )
 }
 
--- src/Components/styled-components/HomeCont.js
+++ src/Components/styled-components/HomeCont.js
@@ -4,13 +4,38 @@ export const Home = styled.div`
 margin-top:40px;
 width:100%;
 z-index:1;
-justify-content:center;
-align-items:center;
-float:center;
-margin-right:30rem;
 
-display:flex;
+@media screen and(max-width:1080px){
+ display:block;
+ flex-direction:column;
+}
+
+
+
 `
-export const Background = styled.img`
+export const Homelg = styled.div`
+display:flex;
+align-items:center;
+justify-content:center;
+flex-direction:'row';
+flex-wrap:wrap;
+float:center;
 
+@media screen and(max-width:760px){
+
+ display:block;
+ flex-direction:column;
+}
+`
+export const Card1 = styled.div`
+margin:100px;
+justify-content: 'center';
+align-items: 'cemter'; 
+display: 'flex'; 
+flex-direction: 'row';
+
+@media screen and(max-width:960px){
+    display:block;
+    flex-direction:column;
+}
 `
